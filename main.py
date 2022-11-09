90fd4b9c18325d8f5b0db1b7b246d97cc8e17d66
Artem Rys <rysartem@gmail.com>
2022-11-09 20:35:51 +0100

    trying to trigger codeql

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ x = 5
 # This is commented code
 
 password = "mysupersecretpassword"
+
+
+class ExplicitReturnInInit(object):
+    def __init__(self, i):
+        self.i = i
+        return self
+
+impot hello
